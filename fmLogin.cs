233824525d0d93b970272415cbd380ad49c6b71e
hansol418 <hslee4954@gmail.com>
2025-06-20 14:03:47 +0900

    RM chapter2 - 4minutes

--- fmLogin.cs
+++ fmLogin.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace RM
 {
-    public partial class Form1: Form
+    public partial class fmLogin: Form
     {
-        public Form1()
+        public fmLogin()
         {
             InitializeComponent();
         }
